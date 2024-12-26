54e384d5584250f73012d56f6f3e09d9d59126ee
Adithi H <adithih03@gmail.com>
2024-12-26 20:09:54 +0530

    next-auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,21 +4,20 @@ import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import React from 'react'
 
-const signup = () => {
+const login = () => {
   return (
     <div className='grid place-content-center min-h-screen bg-gray-100'>
       <div className='flex flex-col justify-center gap-5 items-center py-10 w-[450px] shadow-lg rounded-lg bg-white'>
-        <h1 className='text-center font-bold text-4xl'> Sign Up </h1>
+        <h1 className='text-center font-bold text-4xl'> Login </h1>
         <form action="" className='w-full px-5'>
-          <FormInput type="text" placeholder='Enter your name' label="Full Name" />
           <FormInput type="email" placeholder='Enter your email' label="Email " />
           <FormInput type="password" placeholder='Enter Password' label="Password " />
           <Button type='submit' className='w-full bg-blue-500 mt-5'>Button</Button>
         </form>
-        <Link href="/login" className='text-center text-blue-800 cursor-pointer'> Already have an account? Login </Link>
+        <Link href="/signup" className='text-center text-blue-800 cursor-pointer'> Don't have an account? Sign Up! </Link>
       </div>
     </div>
   )
 }
 
-export default signup
+export default login
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -12,10 +12,10 @@ type Props = {
 const FormInput = ({ type, placeholder, label }: Props) => {
   return (
     <div>
-          <Label htmlFor="name" className='mb-2 text-sm font-medium text-gray-700'>
+          <Label htmlFor="name" className='mb-3 text-sm font-medium text-gray-700'>
             {label}
           </Label>
-          <Input className='w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300' 
+          <Input className='w-full px-3 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300' 
           type={type} placeholder={placeholder} />
     </div>
   )
